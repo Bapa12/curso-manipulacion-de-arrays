9a080486edeb76dea4c0c08dd3f5d38b5fe79ed5
Bapa <bapa.bc04@gmail.com>
2024-03-19 11:21:12 -0300

    Added an exercise of Reduce Method

--- server/05-reduce.js
+++ server/05-reduce.js
@@ -12,3 +12,26 @@ console.log("Original Array:", totals);
 let sum2 = totals.reduce((number, item) => number + item, 0);
 console.log("Sum With Reduce Method:", sum2);
 console.log("Original Array:", totals);
+
+let array = [];
+
+for (let i = 0; i < 30; i++) {
+  array.push(Math.floor(Math.random() * 11));
+}
+
+const newArray = array.reduce((obj, value) => {
+  if (value < 5) {
+    obj["1-4"] += 1;
+  } else if (value < 9) {
+    obj["5-8"] += 1;
+  } else {
+    obj["9-10"] += 1;
+  }
+  return obj;
+}, {
+  "1-4": 0,
+  "5-8": 0,
+  "9-10": 0
+});
+
+console.log(newArray);
